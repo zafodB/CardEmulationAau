dfab22dac2f35251e7f66f66f1618f81db094cd9
Filip <filiplama@gmail.com>
2016-11-15 19:01:23 +0100

    Initial commit

---
+++ app/src/main/java/com/example/filip/cardemulationaau/CardService.java
@@ -0,0 +1,24 @@
+package com.example.filip.cardemulationaau;
+
+import android.app.Service;
+import android.content.Intent;
+import android.nfc.cardemulation.HostApduService;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+
+public class CardService extends HostApduService {
+
+    public static final String TAG = "CardService2";
+
+    @Override
+    public byte[] processCommandApdu(byte[] commandApdu, Bundle extras) {
+        Log.i(TAG, "Hura!");
+        return new byte[0];
+    }
+
+    @Override
+    public void onDeactivated(int reason) {
+
+    }
+}
---
+++ app/src/main/java/com/example/filip/cardemulationaau/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.filip.cardemulationaau;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.nfc.cardemulation.CardEmulation;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
